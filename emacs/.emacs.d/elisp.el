3e949611ee0101ccd1ceffa955c2c5dc4fc51cf3
Arjang Talattof <arjang@magellan.home>
2019-09-07 09:35:55 +0100

    os x branch with normal emacs no more evil

--- emacs/.emacs.d/elisp.el
+++
@@ -1,7 +0,0 @@
-
-(defun my-elisp-mode-hook ()
-  (evil-leader/set-key "me" 'eval-last-sexp)
-  (evil-leader/set-key "mb" 'eval-buffer))
-
-(add-hook 'emacs-lisp-mode-hook 'company-mode)
-(add-hook 'emacs-lisp-mode-hook 'my-elisp-mode-hook)

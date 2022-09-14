2b4cf6cd9a2855b7bf45b9ea68f800b39e423df5
Arjang Talattof <talattof@gmail.com>
2022-09-13 21:06:27 -0700

    [polybar] updated polybar to be more robust like i3status

---
+++ polybar/.config/polybar/vpn.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export VPN="$(nmcli -t -f name,type connection show --order name --active 2>/dev/null | grep vpn | head -1 | cut -d : -f 1)"
+
+
+if [ -n "$VPN" ]; then
+	echo "%{F#aec795}%{F-}[%{F#fAA} $VPN %{F-}]"
+else
+	echo %{F#f00}%{F-}[]
+fi

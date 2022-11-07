7780faa85be9fcaf1889b82d79b465e87a269b13
Arjang Talattof <talattof@gmail.com>
2022-11-07 08:53:17 +0000

    [dotfiles] alacritty polybar updated to work with dwm

--- polybar/.config/polybar/bin/system-nvidia-smi.sh
+++ polybar/.config/polybar/bin/system-nvidia-smi.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+if ! command -v nvidia-smi /dev/null
+then
+    exit
+fi
+
+
 nvidia-smi --query-gpu=utilization.gpu,temperature.gpu,memory.free,memory.total --format=csv,noheader,nounits | awk -F'[,]' '{ gpuf=int(100*$3/1024)/100; gpuu=int(100*$4/1024)/100; print "  "$1"% "$2"°C "gpuf"GiB/"gpuu"GiB"}'

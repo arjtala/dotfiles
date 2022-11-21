41a822bed72875c21f10a09df11f4fb5dbef7565
Arjang Talattof <talattof@gmail.com>
2022-11-21 10:23:23 +0000

    [polybar] fixed nvida-smi output

--- polybar/.config/polybar/bin/system-nvidia-smi.sh
+++ polybar/.config/polybar/bin/system-nvidia-smi.sh
@@ -1,17 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-if ! command -v nvidia-smi /dev/null
+function gpu()
+{
+if ! command -v nvidia-smi &> /dev/null
 then
-    exit
-fi
-
-query=$(nvidia-smi \
-	--query-gpu=utilization.gpu,temperature.gpu,memory.free,memory.total \
-	--format=csv,noheader,nounits)
-
-if [[ $query == *"failed"* ]]; then
-	echo "";
+    echo "";
 else
-	echo $query | awk -F'[,]' '{ gpuf=int(100*$3/1024)/100; gpuu=int(100*$4/1024)/100; print "  "$1"% "$2"°C "gpuf"GiB/"gpuu"GiB"}'
+    local query=$(nvidia-smi)
+	if [[ $query == *"failed"* ]]; then
+		echo "";
+	else
+		echo $(nvidia-smi \
+					  --query-gpu=utilization.gpu,temperature.gpu,memory.free,memory.total \
+					  --format=csv,noheader,nounits) | awk -F'[,]' '{ gpuf=int(100*$3/1024)/100; gpuu=int(100*$4/1024)/100; print "  "$1"% "$2"°C "gpuf"GiB/"gpuu"GiB"}';
+	fi
 fi
+}
 
+gpu

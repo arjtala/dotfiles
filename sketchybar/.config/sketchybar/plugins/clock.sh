2b5e3329319e12665e56f53d9836424fc7a229e0
Arjang Talattof <talattof@gmail.com>
2026-03-27 15:26:16 +0500

    sketchybar: add network, VPN, and athan plugins; fix clock icon
    - Add network plugin showing IP address (wifi/ethernet detection)
    - Add VPN plugin with Cisco Secure Client support
    - Add athan plugin for next prayer time
    - Move clock icon between date and time in label

--- sketchybar/.config/sketchybar/plugins/clock.sh
+++ sketchybar/.config/sketchybar/plugins/clock.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-# Format matches waybar: "Mon, 23 Mar 2026   14:30"
-sketchybar --set "$NAME" label="$(date '+%a, %d %b %Y   %H:%M')"
+sketchybar --set "$NAME" label="$(date "+%a, %d %b %Y    %H:%M")"
